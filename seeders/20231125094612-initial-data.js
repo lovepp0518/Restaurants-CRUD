4d86cc3185daa500faa029aa952b27613cea32cf
lovepp0518 <lovepp0518@gmail.com>
2023-11-29 16:33:53 +0800

    add seeder

---
+++ seeders/20231125094612-initial-data.js
@@ -0,0 +1,20 @@
+'use strict';
+const restaurantsJson = require('./data/restaurant.json').results
+
+// 使用 map 函式為每個物件添加 createdAt 和 updatedAt
+const initialData = restaurantsJson.map(item => ({
+  ...item,
+  createdAt: new Date(),
+  updatedAt: new Date()
+}));
+
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('restaurants', initialData)
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('restaurants', null)
+  }
+};
