3ecb0cde2a9ff8fa0b9c9ac76be4175fab83f27a
lovepp0518 <lovepp0518@gmail.com>
2023-12-19 14:30:27 +0800

    add the registration function

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,40 @@ const express = require('express')
 const router = express.Router()
 
 const db = require('../models')
-const user = db.user
+const User = db.User
 
-router.post('/', (req, res) => {
-  return res.send('註冊成功')
+router.post('/', (req, res, next) => {
+  const { email, name, password, confirmPassword } = req.body
+
+  if (!email || !password) {
+    req.flash('error', 'email 及 password 為必填')
+    return res.redirect('back')
+  }
+
+  if (password !== confirmPassword) {
+    req.flash('error', '驗證密碼與密碼不符')
+    return res.redirect('back')
+  }
+
+  return User.findOrCreate({
+    where: { email },
+    defaults: { name, password }
+  })
+    .then(([user, created]) => {
+      if (!created) {
+        // 如果用户已经存在，返回email已註冊的訊息
+        req.flash('error', 'email已註冊')
+        return res.redirect('back')
+      } else {
+        // 註冊成功
+        req.flash('success', '註冊成功')
+        return res.redirect('/login')
+      }
+    })
+    .catch((error) => {
+      error.errorMessage = '註冊失敗'
+      next(error)
+    })
 })
 
 module.exports = router
