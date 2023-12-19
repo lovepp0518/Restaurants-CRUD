c2c172d62391815b4322f272665759cd3c44863c
lovepp0518 <lovepp0518@gmail.com>
2023-12-19 12:15:05 +0800

    add user seeders

--- seeders/20231125094612-initial-data.js
+++ seeders/20231125094612-initial-data.js
@@ -2,18 +2,37 @@
 const restaurantsJson = require('./data/restaurant.json').results
 
 // 使用 map 函式為每個物件添加 createdAt 和 updatedAt
-const initialData = restaurantsJson.map(item => ({
+const initialData = restaurantsJson.map((item, index) => ({
   ...item,
   createdAt: new Date(),
-  updatedAt: new Date()
+  updatedAt: new Date(),
+  userId: index < 3 ? 1 : 2
 }))
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('restaurants', initialData)
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'user1',
+        email: 'user1@example.com',
+        password: '12345678',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'user2',
+        email: 'user2@example.com',
+        password: '12345678',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]
+    );
+    await queryInterface.bulkInsert('restaurants', initialData);
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('restaurants', null)
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null);
+    await queryInterface.bulkDelete('restaurants', null);
   }
 }
