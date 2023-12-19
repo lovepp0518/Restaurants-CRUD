a1e41043479a7f18254a47c4ebdd645650d5b140
lovepp0518 <lovepp0518@gmail.com>
2023-12-19 10:57:25 +0800

    add model and schema for users table and userId field in restaurants table

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const db = require('../models')
+const user = db.user
+
 router.post('/', (req, res) => {
   return res.send('註冊成功')
 })
